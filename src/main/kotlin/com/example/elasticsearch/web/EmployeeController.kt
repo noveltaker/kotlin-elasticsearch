b545a5d423d3f81403ea06adee6c68feccd3de31
noveltaker <pnw123456@icloud.com>
2022-06-01 13:10:27 +0900

    fix: 프로포티 테스트 케이스 오류 수정

--- src/main/kotlin/com/example/elasticsearch/web/EmployeeController.kt
+++ src/main/kotlin/com/example/elasticsearch/web/EmployeeController.kt
@@ -1,6 +1,6 @@
 package com.example.elasticsearch.web
 
-import com.example.elasticsearch.service.EmployeeService
+import com.example.elasticsearch.service.EmployeeServiceImpl
 import com.example.elasticsearch.service.dto.Created
 import com.example.elasticsearch.service.dto.PageDTO
 import org.springframework.http.HttpStatus
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class EmployeeController constructor(private val service: EmployeeService) {
+class EmployeeController constructor(private val service: EmployeeServiceImpl) {
 
     @PostMapping("employee")
     fun created(@RequestBody dto: Created) = ResponseEntity.status(HttpStatus.CREATED).body(service.save(dto))
--- src/test/kotlin/com/example/elasticsearch/config/ElasticSearchPropertiesTest.kt
+++ src/test/kotlin/com/example/elasticsearch/config/ElasticSearchPropertiesTest.kt
@@ -1,9 +1,9 @@
 package com.example.elasticsearch.config
 
-import com.example.elasticsearch.service.EmployeeService
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.test.autoconfigure.data.elasticsearch.AutoConfigureDataElasticsearch
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa
@@ -18,16 +18,16 @@ import org.springframework.test.context.TestPropertySource
 @EnableConfigurationProperties(ElasticSearchProperties::class)
 @TestPropertySource("classpath:application.yml")
 internal class ElasticSearchPropertiesTest constructor(
-    val elasticSearchProperties: ElasticSearchProperties,
-    val employeeService: EmployeeService
 ) {
 
+    @Autowired
+    private var elasticSearchProperties: ElasticSearchProperties? = null
 
     @Test
     @DisplayName("설정 파일 테스트 케이스")
     fun getProperties() {
-        Assertions.assertEquals(elasticSearchProperties.getConfig().getIp(), "localhost")
-        Assertions.assertEquals(elasticSearchProperties.getConfig().getPort(), "9200")
-        Assertions.assertEquals(elasticSearchProperties.getConfig().getFullUrl(), "localhost:9200")
+        Assertions.assertEquals(elasticSearchProperties?.getConfig()?.getIp(), "localhost")
+        Assertions.assertEquals(elasticSearchProperties?.getConfig()?.getPort(), "9200")
+        Assertions.assertEquals(elasticSearchProperties?.getConfig()?.getFullUrl(), "localhost:9200")
     }
 }
